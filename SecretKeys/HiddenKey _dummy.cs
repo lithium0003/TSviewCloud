95ff8edd8fdf6c36de945b60ff211382e14953cf
lithium0003 <contact@lithium03.info>
2017-11-29 05:17:24 +0900

    add 32bit package

---
+++ SecretKeys/HiddenKey _dummy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretKeys
+{
+    public class HiddenKey
+    {
+        public readonly static string AmazonDrive = "";
+        public readonly static string GoogleDrive = "";
+    }
+}
